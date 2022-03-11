aa7d6b5e759dd8ce886d6b62a62717c0cda1d48b
SatouKz <alexander.ajalla404@gmail.com>
2022-03-11 11:35:32 -0300

    ejercicio-completado

--- commands/index.js
+++ commands/index.js
@@ -1,23 +1,44 @@
-// commands/index.js
 const fs = require("fs");
+const request = require("request");
 
-const pwd = () => {
-  process.stdout.write(process.cwd());
+const done = (output) => {
+  process.stdout.write(output);
+  process.stdout.write("prompt > ");
 };
-const date = () => {
-  process.stdout.write(Date());
+
+const pwd = (file) => {
+  const output = process.cwd();
+  done(output);
 };
-const ls = () => {
+const date = (file) => {
+  const output = Date();
+  done(output);
+};
+const ls = (file) => {
+  let output = "";
   fs.readdir(".", function (err, files) {
     if (err) throw err;
     files.forEach(function (file) {
-      process.stdout.write(file.toString() + "\n");
+      output += file.toString() + "\n";
     });
-    process.stdout.write("prompt > ");
+    done(output);
+  });
+};
+
+const echo = (file) => {
+  const output = file;
+  done(output);
+};
+const curl = (file) => {
+  const output = {};
+  request(file, (err, res, body) => {
+    done(body);
   });
 };
 module.exports = {
   pwd,
   date,
   ls,
+  echo,
+  curl,
 };
